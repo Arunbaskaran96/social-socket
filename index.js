27200749f13e42dcbf77a26c0e75a8c3c85c5b1e
arundhi04101996@gmail.com <arundhi04101996@gmail.com>
2023-08-19 15:03:46 +0530

    social-media-socket

---
+++ index.js
@@ -0,0 +1,54 @@
+const io=require("socket.io")(8900,{
+    cors:{
+        origin:"http://localhost:3000"
+    }
+})
+
+
+let users=[]
+
+function add(userId,socketId){
+    !users.some((user)=>user.userId===userId)&&
+    users.push({userId,socketId})
+}
+
+function remove(socketId){
+    return users= users.filter((user)=>user.socketId!==socketId)
+}
+
+function finduser(receiverId){
+    return users.find((item)=>item.userId===receiverId)
+
+}
+
+io.on("connect",(socket)=>{
+    console.log("User Connected")
+    socket.on("adduser",(user)=>{
+        add(user,socket.id)
+        io.emit("getusers",users)
+        console.log(user)
+        console.log(users)
+
+    })
+
+
+    socket.on("addmessage",({senderId,receiverId,message})=>{
+        console.log({senderId,receiverId,message})
+       const user= finduser(receiverId)
+       io.to(user?.socketId).emit("getmessage",{
+        senderId,message
+       })
+
+    })
+    
+    socket.on("disconnect",()=>{
+        console.log("user disconnected")
+        remove(socket.id)
+        io.emit("getusers",users)
+    })
+})
+
+
+
+
+
